5ff825ca99a2913ca3362524c1d93386fed94190
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-28 15:26:39 +0200

    Create CommonBashFunctions.sh

---
+++ lib/CommonBashFunctions.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# This file defines common BASH functions used in other scripts
+
+# check_variable (arg1) - checks if Variable referenced by arg string is empty, then prints error message and exits,
+#                         otherwise simply returns
+
+function check_variable () {
+  if [ -z "$2" ]
+  then
+    echo "$SCRIPT_NAME: Environment Variable $1 cannot be empty! Exiting..."
+    exit 1
+  fi
+}
