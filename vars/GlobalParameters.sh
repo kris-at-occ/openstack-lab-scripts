18537043dd7c4b9948a34886f58c8c4e5284ae08
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-31 21:29:06 +0200

    Update GlobalParameters.sh

--- vars/GlobalParameters.sh
+++ vars/GlobalParameters.sh
@@ -2,3 +2,7 @@
 
 # This file is used to set Global Parameters as BASH Variables.
 # The file is source early in the Global Installation Sript: install-all.sh
+
+MANAGEMENT_NET_CIDR="10.0.0.0/24"
+CONTROLLER1_HOSTNAME="controller1"
+CONTROLLER1_IPv4="10.0.0.11"
