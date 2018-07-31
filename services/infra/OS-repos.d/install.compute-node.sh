49004f99bd0f5d48aca44239ef6bc57cccaf68f0
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-31 21:51:51 +0200

    Create install.compute-node.sh

---
+++ services/infra/OS-repos.d/install.compute-node.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+SCRIPT_NAME="services/infra/OS-repos.d/install.compute-node.sh"
+
+# This file contains script to Install OpenStack Infrastructure OpenStack Repository files on Compute Node
+# This script is expected to run as root
+
+if (whoami != root)
+then 
+  echo "$SCRIPT_NAME: This script must be run as root"
+  exit 1
+fi
+
+# Source Variables Definition for OpenStack Repository
+
+source services/infra/OS-repos.d/vars.sh 2>/dev/null
+
+# Check if mangdatory Parameter Variables are set
+
+check_variable OPENSTACK_RELEASE
+
+# Install OpenStack Basic Packages and OpenStack Release Repositories Package for Ubuntu - chrony
+DEBIAN_FRONTEND=noninteractive apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common
+DEBIAN_FRONTEND=noninteractive add-apt-repository cloud-archive:$OPENSTACK_RELEASE
+DEBIAN_FRONTEND=noninteractive apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -y install python-openstackclient
+
+
