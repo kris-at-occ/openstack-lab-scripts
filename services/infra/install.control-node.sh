a02249e2f2fa73146604eb47789a932c43494c5f
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-31 21:08:04 +0200

    Create install.control-node.sh

---
+++ services/infra/install.control-node.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# This file contains script to Install OpenStack Infrastructure on Control Node
+
+# Source Variables Definition for Infra Service
+source vars.sh 2>/dev/null
+
+# Source All Subservices of Infra Service
+
+for i in 
